0619f9e761a1707c731e0629f02a18535c507fe8
Crashedmind <crashedmind@gmail.com>
2020-06-01 20:20:22 +0100

    build

---
+++ _static/google_analytics_tracker.js
@@ -0,0 +1,7 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-168177985-1');
